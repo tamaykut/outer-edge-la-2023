9fc0809b47be8ef7a39875c976163f3e0e111187
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-03-19 10:45:45 -0700

    connect contract to add

--- frontend/pages/Ad.js
+++ frontend/pages/Ad.js
@@ -6,8 +6,82 @@ import styles from '../styles/Home.module.css';
 import Chatter from '../components/Chatter';
 import Footer from '../components/Footer';
 import Advertiser from '../components/Advertiser';
+import Caller from "../contract/callerContract.json";
+import {
+  useAccount,
+  usePrepareContractWrite,
+  useContractWrite,
+} from "wagmi";
 
 export default function Ad() {
+
+  const CALLINGCONTRACT = '0xF550146991831Be20872fA4809b23dadCc371C43'
+
+  const callingContractConfig = {
+    address: CALLINGCONTRACT,
+    abi: Caller.abi,
+  };
+
+  //wagmi Calling Contract  //////////////////////////////////
+  const { config: callingConfig, data: callData } = usePrepareContractWrite({
+    ...callingContractConfig,
+    functionName: "setTokenUri",
+    args: ["https://wn2nlt2slbn27yjxuoxd23ufdep7qghrhjkp3o4ogm4y6altlhia.arweave.net/s3TVz1JYW6_hN6OuPW6FGR_4GPE6VP27jjM5jwFzWdA"],  // <<<<<<<<<<<<<<<< Hardcoded Advertisement URI
+    overrides: {
+      gasLimit: 1500000,
+    },  
+    onError(error) {
+      console.log("Error", error);
+    },
+  });
+
+  const {
+    data: callingData,
+    writeAsync: callingContract,
+    isLoading: callingIsLoading,
+    isSuccess: callingIsSuccess,
+  } = useContractWrite(callingConfig);
+
+  const callingStream = async () => {
+    try {
+      let callingTxn = await callingContract?.();
+      setLoading(true);
+      await callingTxn.wait();
+      setLoading(false);
+      setNFTMinted(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //////////////////////////////////////////////////////
+
+  ///Stop Stream ///////////////////////////////////////
+  const { config: stopConfig, data: stopStreamData } = usePrepareContractWrite({
+    ...callingContractConfig,
+    functionName: "stopStream",
+    overrides: {
+      gasLimit: 1500000,
+    },
+    onError(error) {
+      console.log("Error", error);
+    },
+  });
+
+  const { data: stopData, writeAsync: stopStream, isLoading: stopIsLoading, isSuccess: stopIsSuccess } = useContractWrite(stopConfig);
+
+  const stopStreamFunction = async () => {
+    try {
+      let stopTxn = await stopStream?.();
+      setLoading(true);
+      await stopTxn.wait();
+      setLoading(false);
+      setNFTMinted(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };  
+  //////////////////////////////////////////////////////
+
   return (
     <div className="bg-black h-screen w-full ">
       <Head>
@@ -20,11 +94,13 @@ export default function Ad() {
     <div className="flex flex-col items-center justify-center h-screen">
       <button
         className={`bg-donut hover:bg-yellow-600 rounded-full px-12 py-2 text-black font-bold mb-5`}
+        onClick={callingStream}
       >
         Start advertising
       </button>
       <button
         className={`bg-donut hover:bg-yellow-600 rounded-full px-12 py-2 text-black font-bold`}
+        onClick={stopStreamFunction}
       >
         Stop stream
       </button>
